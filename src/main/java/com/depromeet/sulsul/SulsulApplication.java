5fc7020c42fd7f3b518c610444c4ba9f1390a9a2
minsoozz <minsoo1604@naver.com>
2022-03-31 18:51:54 +0900

    feat: 🚀

---
+++ src/main/java/com/depromeet/sulsul/SulsulApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.sulsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulsulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/depromeet/sulsul/SulsulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.sulsul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulsulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
