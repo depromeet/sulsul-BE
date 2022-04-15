09e39894a60850107d51fc6303ab98e75ef15ef5
minsoozz <minsoo1604@naver.com>
2022-04-15 18:43:15 +0900

    🎨 예외 문구 클래스명 변경

--- src/main/java/com/depromeet/sulsul/common/error/exception/custom/MemberNotFoundException.java
+++ src/main/java/com/depromeet/sulsul/common/error/exception/custom/MemberNotFoundException.java
@@ -1,11 +1,12 @@
 package com.depromeet.sulsul.common.error.exception.custom;
 
-import com.depromeet.sulsul.common.error.dto.ErrorCode;
+import static com.depromeet.sulsul.common.error.dto.ErrorType.MEMBER_NOT_FOUND;
+
 import com.depromeet.sulsul.common.error.exception.BusinessException;
 
 public class MemberNotFoundException extends BusinessException {
 
   public MemberNotFoundException() {
-    super(ErrorCode.MEMBER_NOT_FOUND.getMessage());
+    super(MEMBER_NOT_FOUND.getMessage());
   }
 }
