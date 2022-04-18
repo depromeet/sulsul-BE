7324d507f888efdd3b1cb31d018ebd4daa3a0883
Ting-Kim <taeho916@gmail.com>
2022-04-18 22:22:11 +0900

    :sparkles: Model 구현
    - JPA를 이용한 Model 구현
    
    issue: #9

---
+++ src/main/java/com/depromeet/sulsul/domain/member/entity/Flavor.java
@@ -0,0 +1,20 @@
+package com.depromeet.sulsul.domain.member.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Flavor {
+
+    @Id
+    @Column(name = "flavor_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+}
