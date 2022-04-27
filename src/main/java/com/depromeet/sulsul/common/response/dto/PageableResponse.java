6853932cebd1c3f256899edde3107f55bb0285cc
Ting-Kim <taeho916@gmail.com>
2022-04-27 23:26:14 +0900

    :art: 리뷰 내용 2차 반영 (#14)
    pull request: #14

--- src/main/java/com/depromeet/sulsul/common/response/dto/PageableResponse.java
+++ src/main/java/com/depromeet/sulsul/common/response/dto/PageableResponse.java
@@ -1,19 +1,19 @@
 package com.depromeet.sulsul.common.response.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PageableResponse<T> {
 
-    private Boolean hasNext;
+    private Boolean hasNext = false;
     private List<T> contents;
 
-    public PageableResponse(Boolean hasNext, List<T> contents) {
-        this.hasNext = hasNext;
-        this.contents = contents;
-    }
 }
