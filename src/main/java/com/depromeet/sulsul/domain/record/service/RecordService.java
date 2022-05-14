9d5981ec7b424ac16e587fab72b76028f255131c
Ting-Kim <taeho916@gmail.com>
2022-05-15 00:49:25 +0900

    :feat: 이미지 업로드(to AWS S3) API 개발
    - AWS S3 관련 config 작성
    - AWS S3 client 클래스 작성
    - multipart에 대한 사이즈 크기 제한 설정
    
    issue: #30

---
+++ src/main/java/com/depromeet/sulsul/domain/record/service/RecordService.java
@@ -0,0 +1,26 @@
+package com.depromeet.sulsul.domain.record.service;
+
+import com.depromeet.sulsul.common.dto.ImageDto;
+import com.depromeet.sulsul.common.entity.ImageType;
+import com.depromeet.sulsul.common.external.AwsS3ImageClient;
+import com.depromeet.sulsul.util.ImageUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class RecordService {
+
+    private final AwsS3ImageClient awsS3ImageClient;
+
+    public ImageDto uploadImage(MultipartFile multipartFile) {
+        if (!ImageUtil.isValidExtension(multipartFile.getOriginalFilename())) {
+            throw new IllegalArgumentException("[ERROR] Not supported file format.");
+        }
+        return new ImageDto(awsS3ImageClient.upload(multipartFile, ImageType.RECORD));
+    }
+}
