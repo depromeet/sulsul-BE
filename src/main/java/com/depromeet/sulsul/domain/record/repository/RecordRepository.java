e5b531b721e525836f36c520eaafeab3b540c4b2
RyooChan <fbcks97@naver.com>
2022-05-15 02:57:48 +0900

    Revert "Revert "🎨 Record + Record Flavor 조회 및 저장기능 추가""
    This reverts commit cc0fd68ca3ae1469a1ffa8c17b57f3b6106756da.

---
+++ src/main/java/com/depromeet/sulsul/domain/record/repository/RecordRepository.java
@@ -0,0 +1,11 @@
+package com.depromeet.sulsul.domain.record.repository;
+
+import com.depromeet.sulsul.domain.record.entity.Record;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long>, RecordRepositoryCustom, QuerydslPredicateExecutor<Record> {
+
+}
