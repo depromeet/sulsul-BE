0d28a73bd1d61a12e06da2674f203df9272d844d
Ting-Kim <taeho916@gmail.com>
2022-05-17 01:37:27 +0900

    :art: Swagger 의존성 및 설정 파일 추가
    issue: #30

---
+++ src/main/java/com/depromeet/sulsul/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.depromeet.sulsul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.depromeet.sulsul.domain"))
+//                .paths(PathSelectors.ant("/api/**"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
