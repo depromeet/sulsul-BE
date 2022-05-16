22cf07567e4b7785e02c53935f6555ed1479e3ea
Ting-Kim <taeho916@gmail.com>
2022-05-17 01:39:33 +0900

    :art: PR에 대한 Review 내용 반영(개선)
    - final 키워드 추가
    - api 리소스 복수로 변경
    
    issue:#30

--- src/main/java/com/depromeet/sulsul/domain/record/controller/RecordController.java
+++ src/main/java/com/depromeet/sulsul/domain/record/controller/RecordController.java
@@ -3,18 +3,13 @@ package com.depromeet.sulsul.domain.record.controller;
 import com.depromeet.sulsul.common.dto.ImageDto;
 import com.depromeet.sulsul.common.response.dto.ResponseDto;
 import com.depromeet.sulsul.domain.record.service.RecordService;
-import com.depromeet.sulsul.util.ImageUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/records")
@@ -23,8 +18,8 @@ public class RecordController {
 
     private final RecordService recordService;
 
-    @PostMapping("/image")
-    public ResponseDto<ImageDto> uploadImage(@RequestParam("file") MultipartFile multipartFile) throws IOException {
+    @PostMapping("/images")
+    public ResponseDto<ImageDto> uploadImage(@RequestParam("file") MultipartFile multipartFile) {
 
         return ResponseDto.of(recordService.uploadImage(multipartFile));
     }
