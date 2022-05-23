226a6e74c60d24b5d14223a5365e9289f26d5210
minsoozz <minsoo1604@naver.com>
2022-05-24 00:00:26 +0900

    🎨 커스텀 Exception 추가
    issue: #37

---
+++ src/main/java/com/depromeet/sulsul/common/error/exception/custom/BeerNotFoundException.java
@@ -0,0 +1,12 @@
+package com.depromeet.sulsul.common.error.exception.custom;
+
+import com.depromeet.sulsul.common.error.exception.BusinessException;
+
+import static com.depromeet.sulsul.common.error.dto.ErrorType.BEER_NOT_FOUND;
+
+public class BeerNotFoundException extends BusinessException {
+
+    public BeerNotFoundException() {
+        super(BEER_NOT_FOUND.getMessage());
+    }
+}
