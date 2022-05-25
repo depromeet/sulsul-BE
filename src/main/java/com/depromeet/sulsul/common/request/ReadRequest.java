8cb8dd455c44ceecf0d1ac8b1510b495ff5e51ed
Ting-Kim <taeho916@gmail.com>
2022-05-26 00:26:23 +0900

    :art: ReadRequest 개선
    issue: #32

--- src/main/java/com/depromeet/sulsul/common/request/ReadRequest.java
+++ src/main/java/com/depromeet/sulsul/common/request/ReadRequest.java
@@ -6,18 +6,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReadRequest {
 
+  @Nullable
   private String query;   // 검색어
+
+  @Nullable
   private Long cursor;    // 커서 or 오프셋
   private int limit;     // Page 단위 개수
 
-  @JsonProperty("filter")
+  @JsonProperty(value = "filter")
   private Filter filter;  // 필터 조건
+
+  @JsonProperty(value = "sortBy")
   private List<SortCondition> sortBy; // 정렬 조건
 
 }
