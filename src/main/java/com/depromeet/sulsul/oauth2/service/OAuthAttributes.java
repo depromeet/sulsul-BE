e918e53bfe207371f21ff053fae21f0ab29dd7b6
minsoozz <minsoo1604@naver.com>
2022-05-28 13:58:05 +0900

    🎨 카카오 로그인 구현
    issue: #7

--- src/main/java/com/depromeet/sulsul/oauth2/service/OAuthAttributes.java
+++ src/main/java/com/depromeet/sulsul/oauth2/service/OAuthAttributes.java
@@ -1,47 +1,63 @@
 package com.depromeet.sulsul.oauth2.service;
 
+import com.depromeet.sulsul.domain.member.dto.RoleType;
+import com.depromeet.sulsul.domain.member.entity.Member;
+import java.util.Map;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.util.Map;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class OAuthAttributes {
 
-    private static final String KAKAO = "kakao";
-    private static final String NAVER = "naver";
+  private static final String KAKAO = "kakao";
+  private static final String NAVER = "naver";
 
-    private Map<String, Object> attributes;
-    private String nameAttributeKey;
-    private String name;
-    private String email;
-    @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email) {
-        this.attributes = attributes;
-        this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
-        this.email = email;
+  private Map<String, Object> attributes;
+  private String nameAttributeKey;
+  private String name;
+  private String email;
 
+  @Builder
+  public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email) {
+    this.attributes = attributes;
+    this.nameAttributeKey = nameAttributeKey;
+    this.name = name;
+    this.email = email;
+
+  }
+
+  public static OAuthAttributes of(String registrationId, String userNameAttributeName,
+      Map<String, Object> attributes) {
+    switch (registrationId) {
+      case KAKAO:
+        return ofKakao("id", attributes);
+      case NAVER:
+
+      default:
+        throw new IllegalArgumentException();
     }
+  }
 
-    public static  OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
-        switch (registrationId){
-            case KAKAO:
-                return ofKakao("id",attributes);
-            default:
-                throw new IllegalArgumentException();
-        }
-    }
+  private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
 
-    private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes){
-        Map<String, Object> kakaoAccount = (Map<String,Object>) attributes.get("kakao_account");
+    Map<String, Object> response = (Map<String, Object>) attributes.get("kakao_account");
+    Map<String, Object> profile = (Map<String, Object>) response.get("profile");
 
-        kakaoAccount.forEach((k,v) -> System.out.println("k = " + k + " v " + v));
+    return OAuthAttributes.builder()
+        .name((String) profile.get("nickname"))
+        .email((String) response.get("email"))
+        .attributes(attributes)
+        .nameAttributeKey(userNameAttributeName)
+        .build();
+  }
 
-        return OAuthAttributes.builder()
-                .email((String) kakaoAccount.get("email"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
-                .build();
-    }
+  public Member toEntity() {
+    return Member.builder()
+        .name(name)
+        .email(email)
+        .role(RoleType.USER)
+        .build();
+  }
 }
