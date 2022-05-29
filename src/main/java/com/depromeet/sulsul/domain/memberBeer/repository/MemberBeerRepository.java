698fff27e0b8f7125302431f3f4775b489f59c71
RyooChan <fbcks97@naver.com>
2022-05-29 18:53:05 +0900

    🎨 맥주 찜하기/취소 매핑 및 기능 변경
    - Hard delete로 변경 적용
    - 값이 있는 경우와 없는 경우 메세징 처리(200, 400)
    - URI주소 변경

--- src/main/java/com/depromeet/sulsul/domain/memberBeer/repository/MemberBeerRepository.java
+++ src/main/java/com/depromeet/sulsul/domain/memberBeer/repository/MemberBeerRepository.java
@@ -1,12 +1,11 @@
 package com.depromeet.sulsul.domain.memberBeer.repository;
 
 import com.depromeet.sulsul.domain.memberBeer.entity.MemberBeer;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberBeerRepository extends JpaRepository<MemberBeer, Long> {
-  Optional<MemberBeer> findMemberBeerByBeerIdAndMemberId(Long beerId, Long memberId);
-  MemberBeer getMemberBeerByBeerIdAndMemberId(Long beerId, Long memberId);
+  void deleteByBeerIdAndMemberId(Long beerId, Long memberId);
+  boolean existsByBeerIdAndMemberId(Long beerId, Long memberId);
 }
