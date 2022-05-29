8ba98cf2725a9468c785a0bdb544b3a4240a8c65
minsoozz <minsoo1604@naver.com>
2022-05-29 22:46:40 +0900

    🎨 URL 수정
    Member 패키지에 /api 누락되어 추가
    
    issue: #55

--- src/main/java/com/depromeet/sulsul/domain/member/controller/MemberController.java
+++ src/main/java/com/depromeet/sulsul/domain/member/controller/MemberController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/v1/members")
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
 
