2fba48b2df78cb9419ca918f9380579385859435
minsoozz <minsoo1604@naver.com>
2022-05-29 17:24:24 +0900

    🎨 병합 오류 해결
    RecordFlavor Entity, Repository 중복

--- src/main/java/com/depromeet/sulsul/domain/record/repository/RecordFlavorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.depromeet.sulsul.domain.record.repository;
-
-import com.depromeet.sulsul.domain.record.entity.RecordFlavor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordFlavorRepository extends JpaRepository<RecordFlavor,Long> {
-
-}
