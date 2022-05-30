5e9d17f5a2a4a3e9abd53b16160e62e2871eeec7
RyooChan <fbcks97@naver.com>
2022-05-30 23:57:37 +0900

    ✨ API명세 장성
    - API 명세 작성

--- src/main/java/com/depromeet/sulsul/domain/memberBeer/controller/MemberBeerController.java
+++ src/main/java/com/depromeet/sulsul/domain/memberBeer/controller/MemberBeerController.java
@@ -2,6 +2,8 @@ package com.depromeet.sulsul.domain.memberBeer.controller;
 
 import com.depromeet.sulsul.common.response.dto.ResponseDto;
 import com.depromeet.sulsul.domain.memberBeer.service.MemberBeerService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,16 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/beer/heart")
+@Api(tags = "찜하기 관련 API")
 public class MemberBeerController {
 
   private final MemberBeerService memberBeerService;
 
   @PostMapping("/{beerId}")
+  @ApiOperation(value = "찜하기 API")
   public ResponseDto<Boolean> save(@PathVariable("beerId") Long beerId){
     Long memberId = 1L; // TODO : 임시 유저아이디 생성
     return ResponseDto.from(memberBeerService.save(beerId, memberId));
   }
 
+  @ApiOperation(value = "찜하기 취소 API")
   @DeleteMapping("/{beerId}")
   public ResponseDto<Boolean> delete(@PathVariable("beerId") Long beerId){
     Long memberId = 1L; // TODO : 임시 유저아이디 생성
