4dc7586ca33a71aa348f4c5a84b769afb1702b77
minsoozz <minsoo1604@naver.com>
2022-05-31 00:42:52 +0900

    🎨 기록 조회 API 구현
    issue: #61

--- src/main/java/com/depromeet/sulsul/domain/flavor/controller/FlavorController.java
+++ src/main/java/com/depromeet/sulsul/domain/flavor/controller/FlavorController.java
@@ -2,9 +2,8 @@ package com.depromeet.sulsul.domain.flavor.controller;
 
 
 import com.depromeet.sulsul.common.response.dto.ResponseDto;
-import com.depromeet.sulsul.domain.flavor.dto.FlavorResponseDto;
 import com.depromeet.sulsul.domain.flavor.dto.FlavorResponse;
-import com.depromeet.sulsul.domain.flavor.entity.Flavor;
+import com.depromeet.sulsul.domain.flavor.dto.FlavorResponseDto;
 import com.depromeet.sulsul.domain.flavor.service.FlavorService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
--- src/main/java/com/depromeet/sulsul/domain/record/entity/Record.java
+++ src/main/java/com/depromeet/sulsul/domain/record/entity/Record.java
@@ -5,7 +5,6 @@ import com.depromeet.sulsul.domain.beer.entity.Beer;
 import com.depromeet.sulsul.domain.member.entity.Member;
 import com.depromeet.sulsul.domain.record.dto.RecordRequestDto;
 import com.depromeet.sulsul.domain.recordFlavor.entity.RecordFlavor;
-
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.Column;
@@ -48,22 +47,23 @@ public class Record extends BaseEntity {
   @JoinColumn(name = "beer_id")
   private Beer beer;
 
-  @OneToMany(fetch = FetchType.LAZY, mappedBy = "record")
+  @OneToMany(mappedBy = "record")
   private final List<RecordFlavor> recordFlavors = new ArrayList<>();
 
   private String content;
   private Boolean isPublic;
   private Integer feel;
-  private Integer score;
+  private String startCountryKor;
+  private String startCountryEng;
+  private String endCountryKor;
+  private String endCountryEng;
+  private String imageUrl;
+
   @Builder
-  public Record(String content, Boolean isPublic, Integer feel, Integer score) {
+  public Record(String content, Boolean isPublic, Integer feel) {
     this.content = content;
     this.isPublic = isPublic;
     this.feel = feel;
-    this.score = score;
-  }
-  public void updateBeer(Beer beer) {
-    this.beer = beer;
   }
 
   public Record(Member member, Beer beer, RecordRequestDto recordRequestDto) {
@@ -72,5 +72,29 @@ public class Record extends BaseEntity {
     this.content = recordRequestDto.getContent();
     this.isPublic = recordRequestDto.getIsPublic();
     this.feel = recordRequestDto.getFeel();
+  }
+
+  public void updateStartCountry(Record record) {
+    if (record != null) {
+      this.startCountryKor = record.getStartCountryKor();
+      this.startCountryEng = record.getStartCountryEng();
+      return;
+    }
+    this.startCountryKor = "";
+    this.startCountryEng = "";
+  }
+
+  public void updateEndCountry(Beer beer) {
+    if (beer != null) {
+      this.endCountryKor = beer.getNameKor();
+      this.endCountryEng = beer.getNameEng();
+      return;
+    }
+    this.endCountryKor = "";
+    this.endCountryEng = "";
+  }
+
+  public void updateBeer(Beer beer) {
+    this.beer = beer;
   }
 }
