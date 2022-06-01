1f5fbad677c1dbcb33c357429e281afacca46b86
minsoozz <minsoo1604@naver.com>
2022-06-01 16:17:36 +0900

    🎨 사용자 정보 조회 API 구현
    issue: #69

--- src/main/java/com/depromeet/sulsul/domain/member/controller/MemberController.java
+++ src/main/java/com/depromeet/sulsul/domain/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.depromeet.sulsul.domain.member.controller;
 
 import com.depromeet.sulsul.common.response.dto.ResponseDto;
+import com.depromeet.sulsul.domain.member.dto.MemberDto;
 import com.depromeet.sulsul.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +18,11 @@ public class MemberController {
   private final MemberService memberService;
 
   @GetMapping("/{id}")
-  public ResponseEntity<Object> findMember(@PathVariable final Long id) {
-    return ResponseEntity.ok().body(ResponseDto.from(memberService.findMember(id)));
+  public ResponseDto<MemberDto> findMember(@PathVariable final Long id) {
+    return ResponseDto.from(memberService.findMember(id));
+  }
+  @GetMapping("/level/{id}")
+  public ResponseDto<?> findLevelByMemberId(@PathVariable Long id) {
+    return ResponseDto.from(memberService.findLevelByMemberId(id));
   }
 }
