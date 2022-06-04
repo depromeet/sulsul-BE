051e495479919d575eaef2a5f87b039fc951a83b
Ting-Kim <taeho916@gmail.com>
2022-06-05 01:14:57 +0900

    :ambulance: 레포지토리 어노테이션 추가

--- src/main/java/com/depromeet/sulsul/domain/requestBeer/repository/RequestBeerRepositoryCustom.java
+++ src/main/java/com/depromeet/sulsul/domain/requestBeer/repository/RequestBeerRepositoryCustom.java
@@ -2,7 +2,9 @@ package com.depromeet.sulsul.domain.requestBeer.repository;
 
 import com.depromeet.sulsul.domain.requestBeer.dto.RequestBeerResponseDto;
 import java.util.List;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RequestBeerRepositoryCustom {
   List<RequestBeerResponseDto> findByMemberIdWithPageable(Long requestBeerResponseId, Long memberId);
 }
