b7fd8bc09c5998ddab5940c3b6b9793f395db62a
RyooChan <fbcks97@naver.com>
2022-06-05 04:39:29 +0900

    🎨 DTO 출력방식 일부 변경

--- src/main/java/com/depromeet/sulsul/domain/member/dto/MyPageRequestDto.java
+++ src/main/java/com/depromeet/sulsul/domain/member/dto/MyPageRequestDto.java
@@ -14,31 +14,31 @@ public class MyPageRequestDto implements Serializable {
   private Long beerCount;
   private Long recordCount;
   private Long countryCount;
-  private Long memberBeerService;
-  private Long requestBeerService;
+  private Long memberBeerCount;
+  private Long requestbeerCount;
 
   private String name;
 
   @Builder
   public MyPageRequestDto(Long beerCount, Long recordCount, Long countryCount,
-      Long memberBeerService,
-      Long requestBeerService) {
+      Long memberBeerCount,
+      Long requestbeerCount) {
     this.beerCount = beerCount;
     this.recordCount = recordCount;
     this.countryCount = countryCount;
-    this.memberBeerService = memberBeerService;
-    this.requestBeerService = requestBeerService;
+    this.memberBeerCount = memberBeerCount;
+    this.requestbeerCount = requestbeerCount;
   }
 
   public static MyPageRequestDto of(Long beerCount, Long recordCount, Long countryCount,
-      Long memberBeerService,
-      Long requestBeerService) {
+      Long memberBeerCount,
+      Long requestbeerCount) {
     return MyPageRequestDto.builder()
         .beerCount(beerCount)
         .recordCount(recordCount)
         .countryCount(countryCount)
-        .memberBeerService(memberBeerService)
-        .requestBeerService(requestBeerService)
+        .memberBeerCount(memberBeerCount)
+        .requestbeerCount(requestbeerCount)
         .build();
   }
 }
