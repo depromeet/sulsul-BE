58913177bd0a902334c657c7eac03005dae94268
Ting-Kim <taeho916@gmail.com>
2022-06-06 04:32:03 +0900

    :sparkles: 게스트 용 대륙, 국가 API 개발
    issue:#79

---
+++ src/main/java/com/depromeet/sulsul/domain/country/controller/GuestCountryController.java
@@ -0,0 +1,29 @@
+package com.depromeet.sulsul.domain.country.controller;
+
+import com.depromeet.sulsul.common.response.dto.ResponseDto;
+import com.depromeet.sulsul.domain.country.dto.CountryDto;
+import com.depromeet.sulsul.domain.country.service.CountryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags = "국가 APIs (게스트용, version 1)")
+@RequestMapping("/guest/api/v1/countries")
+public class GuestCountryController {
+
+  private final CountryService countryService;
+
+  @GetMapping
+  @ApiOperation(value = "국가 전체 조회 API")
+  public ResponseDto<List<CountryDto>> findAll(
+      @RequestParam(value = "continentId", required = false) Long continentId) {
+    return ResponseDto.from(countryService.findAllByContinentId(continentId));
+  }
+}
