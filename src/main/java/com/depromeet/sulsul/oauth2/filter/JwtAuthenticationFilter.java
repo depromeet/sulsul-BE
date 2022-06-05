b05965e7d54f72a21266024dc53808c511fc87f6
Ting-Kim <taeho916@gmail.com>
2022-06-06 02:43:57 +0900

    :ambulance: SecurityConfig, Jwt 필터 수정

--- src/main/java/com/depromeet/sulsul/oauth2/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/depromeet/sulsul/oauth2/filter/JwtAuthenticationFilter.java
@@ -1,22 +1,22 @@
 package com.depromeet.sulsul.oauth2.filter;
 
 import com.depromeet.sulsul.oauth2.provider.JwtTokenProvider;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
 @RequiredArgsConstructor
-@Component
+@WebFilter(urlPatterns = "/api/*")
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
   private final JwtTokenProvider jwtTokenProvider;
 
   @Override
