d1469abbd4e516b6b2f6467ecfe8ab244b612fa8
Ting-Kim <taeho916@gmail.com>
2022-06-06 16:35:59 +0900

    :art: Swagger - Workaround 설정
    issue:#98

--- src/main/java/com/depromeet/sulsul/config/SwaggerConfig.java
+++ src/main/java/com/depromeet/sulsul/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.depromeet.sulsul.config;
 
+import java.util.Collections;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -7,6 +9,7 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -14,9 +17,19 @@ import springfox.documentation.spring.web.plugins.Docket;
 @EnableOpenApi
 public class SwaggerConfig {
 
+  @Value("${swagger.url}")
+  private String url;
+
+  @Value("${swagger.desc}")
+  private String desc;
+
+  @Value("${spring.profiles.active}")
+  private String profile;
+
   @Bean
   public Docket restApi() {
     return new Docket(DocumentationType.OAS_30)
+        .servers(getServer(profile, url, desc))
         .useDefaultResponseMessages(false)
         .apiInfo(apiInfo())
         .select()
@@ -31,5 +44,9 @@ public class SwaggerConfig {
         .description("sulsul API")
         .version("1.0.0")
         .build();
+  }
+
+  private Server getServer(String profile, String url, String desc) {
+    return new Server(profile, url, desc, Collections.emptyList(), Collections.emptyList());
   }
 }
