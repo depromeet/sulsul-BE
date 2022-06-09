6a54b947e183db9d90802a31ecc69e1298cd3c0d
Ting-Kim <taeho916@gmail.com>
2022-06-10 00:36:30 +0900

    :ambulance: client-url 변경

--- src/main/java/com/depromeet/sulsul/util/CookieUtil.java
+++ src/main/java/com/depromeet/sulsul/util/CookieUtil.java
@@ -44,7 +44,7 @@ public class CookieUtil {
 
   private ResponseCookie createAccessTokenResponseCookie(String token) {
     ResponseCookie cookie = ResponseCookie.from("accessToken", token).path("/").secure(true)
-        .sameSite("None").httpOnly(true).domain(urlOfEnv).maxAge(accessTokenExpirationSecond / 1000)
+        .sameSite("None").httpOnly(true).maxAge(accessTokenExpirationSecond / 1000)
         .build();
 
     return cookie;
