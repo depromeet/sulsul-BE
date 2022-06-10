9bff45544d518c7a04ed73de98c437af5e119126
minsoozz <minsoo1604@naver.com>
2022-06-11 00:07:43 +0900

    🎨 인증객체의 회원 정보로 조회하는 기능 리팩토링

--- src/main/java/com/depromeet/sulsul/domain/memberLevel/controller/MemberLevelController.java
+++ src/main/java/com/depromeet/sulsul/domain/memberLevel/controller/MemberLevelController.java
@@ -3,16 +3,17 @@ package com.depromeet.sulsul.domain.memberLevel.controller;
 import com.depromeet.sulsul.common.response.dto.ResponseDto;
 import com.depromeet.sulsul.domain.memberLevel.dto.MemberLevelResponseDto;
 import com.depromeet.sulsul.domain.memberLevel.service.MemberLevelService;
+import com.depromeet.sulsul.oauth2.User;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/member-levels")
@@ -23,23 +24,21 @@ public class MemberLevelController {
 
   @ApiOperation(value = "전체 level 정보 조회 API")
   @GetMapping("/all")
-  public ResponseDto<List<MemberLevelResponseDto>> findAll(){
+  public ResponseDto<List<MemberLevelResponseDto>> findAll() {
     return ResponseDto.from(memberLevelService.findAll());
   }
 
   @ApiOperation(value = "기록수에 따른 level 조회 API")
   @GetMapping("/{count}")
-  public ResponseDto<MemberLevelResponseDto> findMemberLevelByCount(@PathVariable Long count){
+  public ResponseDto<MemberLevelResponseDto> findMemberLevelByCount(@PathVariable Long count) {
     return ResponseDto.from(memberLevelService.findMemberLevelByCount(count));
   }
 
   @ApiOperation(value = "해당 유저의 level 조회 API")
   @GetMapping
-  public ResponseDto<MemberLevelResponseDto> find(){
-
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    Long memberId = Long.parseLong(authentication.getName());
-    return ResponseDto.from(memberLevelService.find(memberId));
+  public ResponseDto<MemberLevelResponseDto> find(Authentication authentication) {
+    User user = (User) authentication.getPrincipal();
+    return ResponseDto.from(memberLevelService.find(Long.parseLong(user.getUsername())));
   }
 
 }
