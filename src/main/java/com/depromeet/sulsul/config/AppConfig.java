e27331283ecb0a1cd18801c132ea8f86de3c5a55
RyooChan <fbcks97@naver.com>
2022-06-10 22:49:48 +0900

    ✨ xss filter requestbody 적용기능 추가
    - requestBody로 들어온 input data에 확인하는 기능을 부여
    - LocalDateTime 필터링 추가

---
+++ src/main/java/com/depromeet/sulsul/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.depromeet.sulsul.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class AppConfig {
+
+  @Bean
+  @Primary
+  public ObjectMapper objectMapper() {
+    return new Jackson2ObjectMapperBuilder()
+        .featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        .modules(new JavaTimeModule())
+        .timeZone("Asia/Seoul")
+        .build();
+  }
+}
