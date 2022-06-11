6b2e092ecf1eecbc46d034d9a04fae273236f4fb
RyooChan <fbcks97@naver.com>
2022-06-11 16:45:51 +0900

    🚑 해당 유저 최신국가 관련 out data 변경
    - kor / eng 각각 출력

--- src/main/java/com/depromeet/sulsul/domain/record/dto/RecordCountryAndCountResponseDto.java
+++ src/main/java/com/depromeet/sulsul/domain/record/dto/RecordCountryAndCountResponseDto.java
@@ -4,16 +4,19 @@ import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @NoArgsConstructor
 public class RecordCountryAndCountResponseDto {
-  String country;
-  Long count;
+  private String nameKor;
+  private String nameEng;
+  private Long count;
 
   @QueryProjection
-  public RecordCountryAndCountResponseDto(String country, Long count) {
-    this.country = country;
+  public RecordCountryAndCountResponseDto(String nameKor, String nameEng, Long count) {
+    this.nameKor = nameKor;
+    this.nameEng = nameEng;
     this.count = count;
   }
 }
