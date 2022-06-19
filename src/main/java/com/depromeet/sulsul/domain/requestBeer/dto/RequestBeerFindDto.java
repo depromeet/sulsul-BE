730cfbbce10cb8e5cf23b374ad8f6fcdc408a5de
RyooChan <fbcks97@naver.com>
2022-06-19 20:16:02 +0900

    🎨 미등록 맥주 검색 방식 변경

---
+++ src/main/java/com/depromeet/sulsul/domain/requestBeer/dto/RequestBeerFindDto.java
@@ -0,0 +1,15 @@
+package com.depromeet.sulsul.domain.requestBeer.dto;
+
+import com.depromeet.sulsul.util.PaginationUtil;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class RequestBeerFindDto {
+  private Long cursor;
+  private final Integer limit = PaginationUtil.PAGINATION_SIZE+1;
+}
