5ad12ce7b85588576fd164288c7bb715a23535ab
RyooChan <fbcks97@naver.com>
2022-06-21 02:28:59 +0900

    ✨ 미등록 맥주 페이징 로직 추가 및 오타수정

--- src/main/java/com/depromeet/sulsul/domain/requestBeer/dto/RequestBeerFindDto.java
+++ src/main/java/com/depromeet/sulsul/domain/requestBeer/dto/RequestBeerFindDto.java
@@ -5,8 +5,10 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class RequestBeerFindDto {
