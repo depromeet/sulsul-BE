2736621d4d36e337dba68e1de804e5cf31b24e60
Ting-Kim <taeho916@gmail.com>
2022-06-22 02:52:55 +0900

    :ambulance: 반한 맥주 조회 API hotfix - 맥주 중복 조회 개선
    issue:#188

--- src/main/java/com/depromeet/sulsul/domain/beer/repository/BeerRepositoryCustomImpl.java
+++ src/main/java/com/depromeet/sulsul/domain/beer/repository/BeerRepositoryCustomImpl.java
@@ -255,11 +255,12 @@ public class BeerRepositoryCustomImpl implements BeerRepositoryCustom {
   @Override
   public List<BeerResponseDto> findBeerLikedByMemberId(Long memberId) {
     return queryFactory
-        .select(new QBeerResponseDto(country, beer, record.feel, memberBeer)).from(beer)
+        .select(new QBeerResponseDto(country, beer, record.feel.max(), memberBeer)).from(beer)
         .leftJoin(record)
         .on(beer.eq(record.beer).and(record.member.id.eq(memberId))).leftJoin(memberBeer)
         .on(beer.eq(memberBeer.beer).and(memberBeer.member.id.eq(memberId))).innerJoin(country)
         .on(beer.country.eq(country)).where(beer.deletedAt.isNull().and(memberBeer.isNotNull()))
+        .groupBy(country, beer, memberBeer)
         .fetchJoin().fetch();
   }
 
@@ -268,11 +269,12 @@ public class BeerRepositoryCustomImpl implements BeerRepositoryCustom {
     List<SortCondition> sortBy = request.getSortBy();
 
     JPAQuery<BeerResponseDto> jpaQuery = queryFactory
-        .select(new QBeerResponseDto(country, beer, record.feel, memberBeer)).from(beer)
+        .select(new QBeerResponseDto(country, beer, record.feel.max(), memberBeer)).from(beer)
         .leftJoin(record)
         .on(beer.eq(record.beer).and(record.member.id.eq(memberId))).leftJoin(memberBeer)
         .on(beer.eq(memberBeer.beer).and(memberBeer.member.id.eq(memberId))).innerJoin(country)
         .on(beer.country.eq(country)).where(beer.deletedAt.isNull().and(memberBeer.isNotNull()))
+        .groupBy(country, beer, memberBeer)
         .fetchJoin();
 
     jpaQuery = addOrderByWith(jpaQuery, sortBy);
