1eee7929b6a3abcfa22d74b3d9acc1a08a343dec
RyooChan <fbcks97@naver.com>
2022-06-26 21:36:32 +0900

    ✨ 닉네임 변경 valid 로직 추가

--- src/main/java/com/depromeet/sulsul/domain/member/controller/MyPageController.java
+++ src/main/java/com/depromeet/sulsul/domain/member/controller/MyPageController.java
@@ -3,19 +3,24 @@ package com.depromeet.sulsul.domain.member.controller;
 import static com.depromeet.sulsul.util.PropertyUtil.getMemberIdFromPrincipal;
 
 import com.depromeet.sulsul.common.response.dto.ResponseDto;
-import com.depromeet.sulsul.domain.member.dto.MyPageRequestDto;
 import com.depromeet.sulsul.domain.member.dto.MyPageResponseDto;
+import com.depromeet.sulsul.domain.member.dto.NicknameRequestDto;
 import com.depromeet.sulsul.domain.member.facade.MemberFacade;
 import com.depromeet.sulsul.domain.member.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -36,8 +41,8 @@ public class MyPageController {
 
   @ApiOperation(value = "닉네임 update API")
   @PutMapping
-  public ResponseDto<String> updateNickname(@RequestBody @Valid MyPageRequestDto myPageRequestDto, Authentication authentication) {
-    return ResponseDto.from(memberService.updateNickname(getMemberIdFromPrincipal(authentication), myPageRequestDto));
+  public ResponseDto<String> updateNickname(@RequestBody @Valid NicknameRequestDto NicknameRequestDto, Authentication authentication) {
+    return ResponseDto.from(memberService.updateNickname(getMemberIdFromPrincipal(authentication), NicknameRequestDto));
 
   }
 }
