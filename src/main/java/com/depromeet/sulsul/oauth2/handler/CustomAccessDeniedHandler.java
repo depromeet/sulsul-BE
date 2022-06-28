2a938817314c3f99091ba5e40714aeb7248aa3d4
Ting-Kim <taeho916@gmail.com>
2022-06-29 02:48:05 +0900

    :ambulance: hotfix - Jwt 만료와 미인증 처리 로직 분리
    issue:#224

--- src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAccessDeniedHandler.java
@@ -1,22 +1,47 @@
 package com.depromeet.sulsul.oauth2.handler;
 
-import com.depromeet.sulsul.oauth2.User;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.stereotype.Component;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
+import com.depromeet.sulsul.common.response.dto.ResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+  private ObjectMapper objectMapper = new ObjectMapper();
+
   @Override
-  public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-    response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getLocalizedMessage());
+  public void handle(HttpServletRequest request, HttpServletResponse response,
+      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+    int statusWhenDenied = response.getStatus();
+    if (statusWhenDenied == BAD_REQUEST.value()) {
+      processWithErrorResponseDto("[ERROR] jwt가 없거나 잘못 되었습니다.", BAD_REQUEST, response);
+      return;
+    }
+    if (statusWhenDenied == UNAUTHORIZED.value()) {
+      processWithErrorResponseDto("[ERROR] jwt가 만료되었습니다.", UNAUTHORIZED, response);
+      return;
+    }
+
+    response.sendError(HttpServletResponse.SC_FORBIDDEN,
+        accessDeniedException.getLocalizedMessage());
+  }
+
+  private void processWithErrorResponseDto(String errorMessage, HttpStatus httpStatus,
+      HttpServletResponse response) throws IOException {
+    String responseBodyWithJson = objectMapper.writeValueAsString(
+        ResponseDto.ERROR(errorMessage, httpStatus));
+    response.getWriter().write(responseBodyWithJson);
+    response.setStatus(httpStatus.value());
   }
 }
