cff7f69a38e0a168614c71ad007663a3ce5f7edb
Ting-Kim <taeho916@gmail.com>
2022-06-29 03:31:12 +0900

    :ambulance: JwtAuthenticationFilter 이슈 조치
    issue:#224

--- src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAccessDeniedHandler.java
@@ -1,15 +1,9 @@
 package com.depromeet.sulsul.oauth2.handler;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.UNAUTHORIZED;
-
-import com.depromeet.sulsul.common.response.dto.ResponseDto;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -17,31 +11,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
-  private ObjectMapper objectMapper = new ObjectMapper();
-
   @Override
   public void handle(HttpServletRequest request, HttpServletResponse response,
       AccessDeniedException accessDeniedException) throws IOException, ServletException {
 
-    int statusWhenDenied = response.getStatus();
-    if (statusWhenDenied == BAD_REQUEST.value()) {
-      processWithErrorResponseDto("[ERROR] jwt가 없거나 잘못 되었습니다.", BAD_REQUEST, response);
-      return;
-    }
-    if (statusWhenDenied == UNAUTHORIZED.value()) {
-      processWithErrorResponseDto("[ERROR] jwt가 만료되었습니다.", UNAUTHORIZED, response);
-      return;
-    }
-
     response.sendError(HttpServletResponse.SC_FORBIDDEN,
         accessDeniedException.getLocalizedMessage());
-  }
-
-  private void processWithErrorResponseDto(String errorMessage, HttpStatus httpStatus,
-      HttpServletResponse response) throws IOException {
-    String responseBodyWithJson = objectMapper.writeValueAsString(
-        ResponseDto.ERROR(errorMessage, httpStatus));
-    response.getWriter().write(responseBodyWithJson);
-    response.setStatus(httpStatus.value());
   }
 }
--- src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,12 @@
 package com.depromeet.sulsul.oauth2.handler;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
+import com.depromeet.sulsul.common.response.dto.ResponseDto;
+import com.depromeet.sulsul.oauth2.filter.JwtAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -12,8 +19,39 @@ import java.io.IOException;
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+  private ObjectMapper objectMapper = new ObjectMapper();
+
   @Override
-  public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException, ServletException {
+
+    Integer jwtParseResultWhenDenied = Integer.parseInt(
+        response.getHeader(JwtAuthenticationFilter.JWT_PARSE_RESULT));
+
+    setResponseJsonType(response);
+
+    if (jwtParseResultWhenDenied == BAD_REQUEST.value()) {
+      processWithErrorResponseDto("[ERROR] jwt가 없거나 잘못 되었습니다.", BAD_REQUEST, response);
+      return;
+    }
+    if (jwtParseResultWhenDenied == UNAUTHORIZED.value()) {
+      processWithErrorResponseDto("[ERROR] jwt가 만료되었습니다.", UNAUTHORIZED, response);
+      return;
+    }
+
     response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getLocalizedMessage());
+  }
+
+  private void processWithErrorResponseDto(String errorMessage, HttpStatus httpStatus,
+      HttpServletResponse response) throws IOException {
+    String responseBodyWithJson = objectMapper.writeValueAsString(
+        ResponseDto.ERROR(errorMessage, httpStatus));
+    response.getWriter().write(responseBodyWithJson);
+    response.setStatus(httpStatus.value());
+  }
+
+  private void setResponseJsonType(HttpServletResponse response) {
+    response.setContentType("application/json");
+    response.setCharacterEncoding("utf-8");
   }
 }
