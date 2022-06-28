72d3042f56d849762b29137e32921cadae6aa6b4
Ting-Kim <59888684+Ting-Kim@users.noreply.github.com>
2022-06-29 03:40:49 +0900

    🚑 Jwt 필터 등 security 이슈로 인한 문제 조치 (#236)
    * yml 재반영
    
    * 🔥 yml 재삭제
    
    * :ambulance: hotfix - Jwt 만료와 미인증 처리 로직 분리
    
    issue:#224
    
    * :ambulance: JwtAuthenticationFilter 이슈 조치
    
    issue:#224
    
    Co-authored-by: zeze <thwjd2717@gmail.com>

--- src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAccessDeniedHandler.java
+++ src/main/java/com/depromeet/sulsul/oauth2/handler/CustomAccessDeniedHandler.java
@@ -1,22 +1,21 @@
 package com.depromeet.sulsul.oauth2.handler;
 
-import com.depromeet.sulsul.oauth2.User;
-import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.stereotype.Component;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
   @Override
-  public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-    response.sendError(HttpServletResponse.SC_FORBIDDEN, accessDeniedException.getLocalizedMessage());
+  public void handle(HttpServletRequest request, HttpServletResponse response,
+      AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+    response.sendError(HttpServletResponse.SC_FORBIDDEN,
+        accessDeniedException.getLocalizedMessage());
   }
 }
