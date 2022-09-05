d1c967ac0d8cfb6551023d996299e2e52502ea02
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-09-05 22:15:25 +0900

    ✨Async Config

---
+++ src/main/java/com/depromeet/sulsul/config/AsyncConfig.java
@@ -0,0 +1,10 @@
+package com.depromeet.sulsul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig extends AsyncConfigurerSupport {
+}
