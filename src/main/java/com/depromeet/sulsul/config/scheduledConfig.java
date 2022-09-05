0e2544275626cf6d5b10f474c529a2bc99cf15af
RyooChan <53744363+RyooChan@users.noreply.github.com>
2022-09-05 22:13:43 +0900

    Update scheduledConfig.java

--- src/main/java/com/depromeet/sulsul/config/scheduledConfig.java
+++ src/main/java/com/depromeet/sulsul/config/scheduledConfig.java
@@ -1,4 +1,4 @@
-package com.beerair.core.config;
+package com.depromeet.sulsul.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
